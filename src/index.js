5ecbb2b63ef20e9b357c7844514fa77a4e0823a2
Jack <skelton.jt9@gmail.com>
2022-01-15 11:39:05 -0500

    added basic apicall function and get elements func

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import $ from "jquery";
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+import CurrencyConverter from "./currency";
 
+async function makeApiCall(currency){
+  const response = await CurrencyConverter.convert(currency);
+  getElements(response);
+}
+
+function getElements(reposnse){
+  if(reposnse.conversion_rates){
+    console.log(reposnse.conversion_rates)
+  }
+}
+
+$(document).ready(function () {
+  
+}
