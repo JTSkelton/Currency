693ec568d6269800bc298ad9b4186db04aebc4a1
Jack <skelton.jt9@gmail.com>
2022-01-15 11:26:33 -0500

    Added CurrencyConverter Class to query API

---
+++ src/currency.js
@@ -0,0 +1,15 @@
+export default class CurrencyConverter {
+  static async convert() {
+    try {
+      const response = await fetch(
+        `https://v6.exchangerate-api.com/v6/4b893e55c028ab4287796be3/latest/USD`
+      );
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json;
+    } catch (error) {
+      return error.message;
+    }
+  }
+}
