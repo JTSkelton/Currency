b7d6d021e0f2990d26d41ab1d40976e72b8263af
Jack <skelton.jt9@gmail.com>
2022-01-15 17:29:22 -0500

    refined DOM function, added name of $

--- src/index.js
+++ src/index.js
@@ -14,9 +14,8 @@ async function makeApiCall(currency) {
 }
 
 function getElements(response) {
-  if (response.conversion_rates) {
-    console.log(response.conversion_rates);
-  } else {
+  if (response.conversion_rates) return;
+  else {
     $("#showCurrency").text(`There was an error: ${response}`);
   }
 }
@@ -25,9 +24,10 @@ function makeCurrencyList() {
   let myMoneyParse = JSON.parse(sessionStorage.getItem("currencyObject"));
   for (let key in myMoneyParse) {
     const newOption = document.createElement("option");
-    newOption.value = myMoneyParse[key];
+    newOption.value = [key, myMoneyParse[key]];
     newOption.text = key;
     newOption.id = key;
+    console.log(newOption);
     document.getElementById("currencyList").appendChild(newOption);
   }
 }
@@ -39,10 +39,8 @@ window.onload = function () {
 
 $(document).ready(function () {
   $("#convertButton").click(function () {
-    let conversionValue = $("#currencyList").val();
-    let conversionName = $("#currencyList").text();
-    console.log(typeof conversionName);
+    let conversionValue = $("#currencyList").val().split(",");
     let money = $("#usdInput").val();
-    $("#showCurrency").html("Your USD is worth: " + money * conversionValue);
+    $("#showCurrency").html(`Your $${money} USD is worth: ` + money * conversionValue[1] + " In " + conversionValue[0]);
   });
 });
